b0e16e208e0eb5e497c679588a95c88938d259d8
王希宁 <xiningwang120@gmail.com>
2018-04-17 11:31:13 -0500

    Add Book, Shelf Component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  const bookList = [{
+    bookName: 'To Kill a Mockingbird',
+    authorName: 'Harper Lee',
+    bookCoverUrl: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+  }, {
+    bookName: 'To Kill a Mockingbird',
+    authorName: 'Harper Lee',
+    bookCoverUrl: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+  }];
+
   render() {
     return (
       <div className="app">
---
+++ src/Shelf.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Book from './Book';
+
+class Shelf extends Component {
+
+  static propTypes = {
+    shelfName: PropTypes.string.isRequired,
+    bookList: PropTypes.array.isRequired
+  }
+  
+  render() {
+    const { shelfName, bookList } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {bookList.map((book) => (
+              <li>
+                <Book
+                  bookName={ book.name }
+                  authorName={ book.author }
+                  bookCoverUrl={ book.url }/>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Shelf;
