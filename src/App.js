0829565195c5020f674026be625a807447fa5aa4
王希宁 <xiningwang120@gmail.com>
2018-04-17 17:25:18 -0500

    Add: SearchBooks Component and basic routes between main Component and SearchBooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import { Route, BrowserRouter, Link } from 'react-router-dom';
 import './App.css'
+
+// import * as BooksAPI from './BooksAPI';
 import Shelf from './Shelf';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    curReadingBookList: [],
+    wantReadBookList: [],
+    readBookList: [],
     showSearchPage: false
   }
-
-
 
   render() {
     const bookList = [{
@@ -27,46 +25,31 @@ class BooksApp extends React.Component {
       'bookCoverUrl': 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
     }];
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
+      <BrowserRouter>
+        <div className="app">
+            <Route exact path='/' render={() => (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                  <div>
+                    <Shelf shelfName='Currently Reading' bookList={bookList} />
+                    <Shelf shelfName='Want to Reading' bookList={bookList} />
+                    <Shelf shelfName='Read' bookList={bookList} />
+                  </div>
+                </div>
+                <div className="open-search">
+                  <Link to='search'>Add a book</Link>
+                </div>
               </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Shelf shelfName='Currently Reading' bookList={bookList} />
-                <Shelf shelfName='Want to Reading' bookList={bookList} />
-                <Shelf shelfName='Read' bookList={bookList} />
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
-      </div>
+              )} />
+
+            <Route path='/search' render={() => (
+              <SearchBooks />
+            )} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
